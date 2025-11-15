d04198fa10d68867aaa1496b86b36130db800edc
juhyeon3856 <73976487+juhyeon3856@users.noreply.github.com>
2025-11-15 17:41:09 +0900

    feat: init

---
+++ src/main/java/com/smartdistance/sdd/SmartDistanceDispatchApplication.java
@@ -0,0 +1,13 @@
+package com.smartdistance.sdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDistanceDispatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDistanceDispatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartdistance/sdd/SmartDistanceDispatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartdistance.sdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDistanceDispatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
